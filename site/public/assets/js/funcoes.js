289e21e45f7b7d93374246cb70e4896aa4733d56
lean-dro <72211521+lean-dro@users.noreply.github.com>
2023-05-12 23:53:46 -0300

    Finalização de curtidas

--- site/public/assets/js/funcoes.js
+++ site/public/assets/js/funcoes.js
@@ -3,11 +3,16 @@ function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
 
-    var b_usuario = document.getElementById("b_usuario");
+   
 
     if (email != null && nome != null) {
        
     } else {
         window.location = "../login.html";
     }
+}
+
+function sair() {
+    sessionStorage.clear();
+    window.location = "../login.html";
 }
